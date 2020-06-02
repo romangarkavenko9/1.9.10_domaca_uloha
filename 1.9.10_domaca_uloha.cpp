fef746f3822af111f0faf739041f64279bca51f3
kingroma99 <62663520+kingroma99@users.noreply.github.com>
2020-06-02 16:37:04 +0200

    pokus o funkciu mat_destroy a o proceduru mat_random

--- 1.9.10_domaca_uloha.cpp
+++ 1.9.10_domaca_uloha.cpp
@@ -9,7 +9,7 @@ float *elem;
 
 MAT* mat_create_with_type(unsigned int rows, unsigned int cols){
 
-MAT* m=(MAT*)malloc(sizeof(MAT)*rows*cols);
+MAT* m=(MAT*)malloc(sizeof(float)*rows*cols);
 
 if( m == NULL ){
 free(m);
@@ -20,10 +20,36 @@ return m;
 }
 }
 
+void mat_destroy(MAT *mat){
+free(mat);
+}
+
+void mat_random(MAT *mat){
+unsigned int rows,cols;
+MAT* s;
+s=mat_create_with_type( rows, cols);
+int i,j;
+float a;
+	for(i=0;i<rows;i++){
+		for(j=0;j<cols;j++){
+			a=((float)rand() / (RAND_MAX))-0.5;
+			printf("%f ",a);	
+		}
+	}
+}
+
+
+
 int main(){
 unsigned int a,b;
 MAT* m;
 int* c;
-mat_create_with_type(a,b);
+a=3;
+b=2;
+m=mat_create_with_type(a,b);
+printf("%d",m);
+mat_random(m);
+mat_destroy(m);
+printf(" %d",m);
 }
 
