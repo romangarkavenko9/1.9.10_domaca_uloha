9813e171422553bb05ed78818714963789ae35b8
kingroma99 <62663520+kingroma99@users.noreply.github.com>
2020-06-03 22:55:22 +0200

    program pocita determinant matice NxN

--- 1.9.10_domaca_uloha.cpp
+++ 1.9.10_domaca_uloha.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 typedef struct{
 	unsigned int rows;
 	unsigned int cols;
@@ -10,7 +11,7 @@ typedef struct{
 
 MAT* mat_create_with_type(unsigned int rows, unsigned int cols){
 
-	MAT* m=(MAT*)malloc(sizeof(float)*rows*cols);
+	MAT* m=(MAT*)malloc(sizeof(MAT)*rows*cols);
 
 	if( m == NULL )
 	{
@@ -66,7 +67,9 @@ void mat_random(MAT *mat){
 void mat_print(MAT *mat){
 	
 	int i,j;
+	
 	mat_unit(mat);
+	
 		for(i=0;i<mat->rows;i++)
 		{
 			for(j=0;j<mat->cols;j++)
@@ -77,21 +80,111 @@ void mat_print(MAT *mat){
 			printf("\n");
 		}
 		printf("\n");
+		
 	mat_random(mat);
+	
 		for(i=0;i<mat->rows;i++)
 		{
 			for(j=0;j<mat->cols;j++)
-			{
-				printf(" %f ",ELEM(mat,i,j));
+			{   if(ELEM(mat,i,j)<0)
+				{
+				printf("%.3f  ",ELEM(mat,i,j));	
+				}
+				else
+				printf(" %.3f  ",ELEM(mat,i,j));
 			}
 			
 			printf("\n");
 		}
+	printf("\n");
 }
+
+float det_pre_2x2(MAT *mat){
+	float ab;
+	
+	if(mat->cols==2 && mat->rows==2 )
+	{
+		ab=ELEM(mat,0,0)*ELEM(mat,1,1)-ELEM(mat,0,1)*ELEM(mat,1,0);
+	}
+		
+}
+
+
+
+void swap(float *a, float *b) 
+{ 
+    float t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+
+
+
+float mat_determinant(MAT *mat){
+  
+float cislo1,cislo2,det = 1,celok = 1;
+int g;    
+        
+      
+   float pole[mat->rows + 1];   
+        
+     
+    for(int i = 0; i < mat->rows; i++)    
+    {  
+        g = i;   
+            
+        while(ELEM(mat,g,i) == 0 && g < mat->rows) {   
+            g++;       
+                
+        }   
+        if(g == mat->rows)   
+        {     
+            continue;          
+        }   
+        if(g != i)   
+        {     
+            for(int j = 0; j < mat->rows; j++)   
+            {   
+                swap(&ELEM(mat,g,j),&ELEM(mat,i,j));      
+            }   
+                 
+                det = det*pow(-1,g-i);     
+       }   
+             
+       for(int j = 0; j < mat->rows; j++)   
+       {   
+           pole[j] = ELEM(mat,i,j);        
+       }   
+    
+       for(int j = i+1; j < mat->rows; j++)   
+       {   
+           cislo1 = pole[i];    
+           cislo2 = ELEM(mat,j,i);    
+                  
+           for(int k = 0; k < mat->rows; k++)   
+           {      
+               ELEM(mat,j,k) = (cislo1 * ELEM(mat,j,k)) - (cislo2 * pole[k]);           
+           }   
+           celok = celok * cislo1;    
+           }   
+            
+    }   
+      
+    for(int i = 0; i < mat->rows; i++)   
+    {   
+        det = det * ELEM(mat,i,i);   
+            
+    }  
+    return (det/celok);  
+    }   
+
+
 
 int main(){
 	
 	unsigned int a,b;
+	float ad;
 	MAT* m;
 	
 	printf("zadaj pocet riadkov a pocet stlpcov:");
@@ -104,6 +197,10 @@ int main(){
 	m->elem=pole;
 
 	mat_print(m);
+	ad=mat_determinant(m);
+	printf("determinant matice= %.3f",ad);
+
 	mat_destroy(m);
+	
 }
 
